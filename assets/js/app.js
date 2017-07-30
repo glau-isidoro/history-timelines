729688fcccf1f55bad8f610937725c633732c5ca
glau-isidoro <glaucia.isidoro@gmail.com>
2017-07-30 20:10:38 -0300

    mostrando uma timeline

---
+++ assets/js/app.js
@@ -0,0 +1,318 @@
+//var andes = jQuery.getJSON('assets/js/json/andes.json');
+
+var andes = [
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1513,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Descubrimiento del Mar del Sur",
+		   "Hito": 1,
+		   "Descripción": "Descubrimiento del Mar del Sur",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1521,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Desembarco de Magallanes en Filipinas",
+		   "Hito": 1,
+		   "Descripción": "Desembarco de Magallanes en Filipinas",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1521,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Descubrimiento del Amazonas por Orellana",
+		   "Hito": 0,
+		   "Descripción": "Descubrimiento del Amazonas por Orellana",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1532,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Conquista del Perú por Pizarro",
+		   "Hito": 1,
+		   "Descripción": "Conquista del Perú por Pizarro",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1532,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Caída de Atahualpa",
+		   "Hito": 0,
+		   "Descripción": "Caída de Atahualpa",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1536,
+		   "Fecha Inicio": 1536,
+		   "Fecha Fin": 1572,
+		   "Hecho": "Dinastía de los indios Vilcabamba",
+		   "Hito": 0,
+		   "Descripción": "Dinastía de los indios Vilcabamba",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1541,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Fundación de Santiago de Chile por Valdivia",
+		   "Hito": 1,
+		   "Descripción": "Fundación de Santiago de Chile por Valdivia",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1541,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Fundación de Potosí",
+		   "Hito": 1,
+		   "Descripción": "Fundación de Potosí",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1551,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Fundación de la Universidad de San Marcos de Lima",
+		   "Hito": 1,
+		   "Descripción": "Fundación de la Universidad de San Marcos de Lima",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1556,
+		   "Fecha Inicio": 1556,
+		   "Fecha Fin": 1560,
+		   "Hecho": "Gobierno del Virrey Andrés Hurtado de Mendoza",
+		   "Hito": 0,
+		   "Descripción": "Gobierno del Virrey Andrés Hurtado de Mendoza",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1560,
+		   "Fecha Inicio": 1560,
+		   "Fecha Fin": 1564,
+		   "Hecho": "Gobierno del Virrey Diego López Velasco",
+		   "Hito": 0,
+		   "Descripción": "Gobierno del Virrey Diego López Velasco",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1564,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Gobierno del Virrey Hernando de Saavedra",
+		   "Hito": 0,
+		   "Descripción": "Gobierno del Virrey Hernando de Saavedra",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1564,
+		   "Fecha Inicio": 1564,
+		   "Fecha Fin": 1569,
+		   "Hecho": "Gobierno del Virrey Lope García de Castro",
+		   "Hito": 0,
+		   "Descripción": "Gobierno del Virrey Lope García de Castro",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1569,
+		   "Fecha Inicio": 1569,
+		   "Fecha Fin": 1580,
+		   "Hecho": "Gobierno del Virrey Francisco de Toledo",
+		   "Hito": 0,
+		   "Descripción": "Gobierno del Virrey Francisco de Toledo",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1572,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Tupac Amaro ajusticiado",
+		   "Hito": 1,
+		   "Descripción": "Tupac Amaro ajusticiado",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1600,
+		   "Fecha Inicio": 1600,
+		   "Fecha Fin": 1615,
+		   "Hecho": "Redacción de Nueva Crónica de Diego Huamán Poma de Ayala",
+		   "Hito": 1,
+		   "Descripción": "Redacción de Nueva Crónica de Diego Huamán Poma de Ayala",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 },
+		 {
+		   "Linha de tempo": "ANDES",
+		   "Fecha": 1609,
+		   "Fecha Inicio": null,
+		   "Fecha Fin": null,
+		   "Hecho": "Los comentarios reales por inca Garcilaso de la Vega",
+		   "Hito": 1,
+		   "Descripción": "Los comentarios reales por inca Garcilaso de la Vega",
+		   "Antecedentes": "",
+		   "Consecuentes": "",
+		   "Etiquetas": "",
+		   "Fuentes": "",
+		   "Links": "",
+		   "Imagen": ""
+		 }
+		];
+function arredondar(ano) {
+	seculo = ano.toString().slice(0, 2);
+	decada = ano.toString().slice(2, 4);
+	if(decada < 25) {
+		decada = "00";
+	} else if(decada >= 25 && decada < 50) {
+		decada = "25";
+	} else if(decada >= 50 && decada < 75) {
+		decada = "50";
+	} else if(decada >= 75) {
+		decada = "75";
+	}
+	return parseInt(seculo + decada);
+}
+
+var indiceMultiplicacao = 15;
+
+var years = [];
+
+for(i=0;i<andes.length;i++){
+	years.push(andes[i].Fecha);
+}
+
+years.sort();
+var comeco = arredondar(years[0]);
+var final = arredondar(years[years.length-1]);
+
+var timelineYears = years[years.length-1]-comeco;
+
+var timelineSize = ''+ ((timelineYears + 20) * indiceMultiplicacao)+'px';
+
+var timelineHTML = '<hr style="border-width: 1px; width:'+ timelineSize +';">';
+
+$('#timelines').css('width', timelineSize);
+
+for(i=0;i<timelineYears;i+=25) {
+
+	var leftPos = (i+10) * indiceMultiplicacao;
+	var lineYear = (i==0)? comeco : comeco+i;
+
+	timelineHTML += '<div style="height:300px;width:1px;background-color:#ccc;position:absolute;top:0;left:'+leftPos+'px;"></div>';
+	timelineHTML += '<div style="height:300px;width:1px;background-color:#ccc;position:absolute;top:35px;left:'+leftPos+'px;">'+lineYear+'</div>';
+
+}
+
+timelineHTML += '<div style="height:300px;width:1px;background-color:#ccc;position:absolute;top:0;left:'+((final-comeco)+10)*indiceMultiplicacao+'px;"></div>';
+
+for(i=0;i<years.length;i++){
+
+	var color = (years[i] == years[i-1]) ? '#0ff': '#00f';
+
+	var lugarDaBola = (((years[i] - comeco) + 10)*indiceMultiplicacao)-7.5;
+
+	timelineHTML += '<div style="height:30px;width:30px;;position:absolute;top:80px;left:'+ lugarDaBola +'px;">'+years[i]+'</div>';
+	timelineHTML += '<div style="height:15px;width:15px;border-radius:100%;background-color:'+color+';position:absolute;top:100px;left:'+ lugarDaBola +'px;"></div>';
+
+}
+
+$('#timelines').html(timelineHTML);
